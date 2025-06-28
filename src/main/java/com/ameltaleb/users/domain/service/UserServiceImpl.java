099cab6ac369710c3626bac0616b85af2d5db460
amelinfo <taleb.amelinfo@gmail.com>
2025-06-28 06:01:04 +0200

    feat/question1:add primary adapter

---
+++ src/main/java/com/ameltaleb/users/domain/service/UserServiceImpl.java
@@ -0,0 +1,16 @@
+package com.ameltaleb.users.domain.service;
+
+import java.util.List;
+
+import com.ameltaleb.users.domain.model.User;
+import com.ameltaleb.users.domain.port.in.UserService;
+
+public class UserServiceImpl implements UserService{
+
+    @Override
+    public List<User> getAllUsers() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getAllUsers'");
+    }
+
+}
